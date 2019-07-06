1b2bdb07cce88bd941aec41552cda08b4260d52e
Ryota <unicoonn1@yahoo.co.jp>
2019-07-06 18:24:34 +0900

    change works icon size

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 $(function(){
   $('.title').click(function(){
-    if ($(this).text() === "eBooks") {
-      window.open("https://apps.apple.com/jp/app/読書管理アプリ-ebooks/id1247081342");
-    }else if($(this).text() === "アニメリスト"){
-      window.open("https://apps.apple.com/jp/app/アニメリスト/id1202118481");
-    }else if($(this).text() === "ポートフォリオサイト"){
-      window.open('file:///Users/unicoonn1/Desktop/portfolio/unicoonn1.github.io/index.html#');
-    }
+    $('.title').click(function(){
+      $('.title').text("タップしたよ");
+    });
   });
 
 
