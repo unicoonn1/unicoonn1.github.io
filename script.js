f00dce123ba79e3968e7daf907ffb25ba2c68587
Ryota <unicoonn1@yahoo.co.jp>
2019-07-07 03:07:57 +0900

    add header white remove

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(function(){
   $('.header-right a').click(function(e){
     e.preventDefault();
+    $(this).css('color','white');
     var headerHeight = 65;
     var id = $(this).attr('href');
     var position = $(id).offset().top;
