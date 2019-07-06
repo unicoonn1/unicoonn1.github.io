a45eb225d3274687ce7fbaa43a52dc93f86ebbb8
Ryota <unicoonn1@yahoo.co.jp>
2019-07-07 03:51:36 +0900

    add header-right activeMethod

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 $(function(){
   $('.header-right a').click(function(e){
     e.preventDefault();
-    $(this).css('color','white');
     var headerHeight = 65;
     var id = $(this).attr('href');
     var position = $(id).offset().top;
